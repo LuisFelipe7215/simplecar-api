45461ea780542689260fc52758492c4f875abb50
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-11-11 14:20:44 -0300

    feat: cria o projeto

---
+++ src/main/java/com/luisfelipe/simplecarapi/SimplecarApiApplication.java
@@ -0,0 +1,13 @@
+package com.luisfelipe.simplecarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplecarApiApplication.class, args);
+    }
+
+}
