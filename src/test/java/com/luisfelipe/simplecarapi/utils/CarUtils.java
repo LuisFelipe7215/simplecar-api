c91aab1b1554131eca118a785a4590b094ceaa8b
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-11-14 16:15:21 -0300

    refactor: altera o modificador do método

--- src/test/java/com/luisfelipe/simplecarapi/utils/CarUtils.java
+++ src/test/java/com/luisfelipe/simplecarapi/utils/CarUtils.java
@@ -1,13 +1,15 @@
 package com.luisfelipe.simplecarapi.utils;
 
 import com.luisfelipe.simplecarapi.domain.Car;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class CarUtils {
 
-    public static List<Car> createCarsList(){
+    public List<Car> newCarsList(){
         Car corolla = Car.builder().id(1L).type("SUV").brand("Toyota").model("Corolla").year(2022).price(20000.00).build();
         Car civic = Car.builder().id(2L).type("SUV").brand("Honda").model("Civic").year(2022).price(20000.00).build();
         Car golf = Car.builder().id(3L).type("SUV").brand("Volkswagen").model("Golf").year(2022).price(20000.00).build();
@@ -15,7 +17,7 @@ public class CarUtils {
         return new ArrayList<>(List.of(corolla, civic, golf));
     }
 
-    public static Car newCarToSave(){
+    public Car newCarToSave(){
         return Car.builder().id(1L).type("Sedan").brand("Honda").model("Accord").year(2025).price(60000.00).build();
     }
 }
