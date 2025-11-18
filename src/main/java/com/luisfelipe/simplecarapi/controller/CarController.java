c3421506b64643b8c6413e5309020ea8171b7d23
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-11-18 16:40:37 -0300

    refactor: remove a barra do uri

--- src/main/java/com/luisfelipe/simplecarapi/controller/CarController.java
+++ src/main/java/com/luisfelipe/simplecarapi/controller/CarController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/cars")
+@RequestMapping("v1/cars")
 @RequiredArgsConstructor
 @Log4j2
 public class CarController {
