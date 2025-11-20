f9289509d840143c79249fcc808d155d97397350
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-11-20 11:49:08 -0300

    refactor: adiciona o FileUtils para utilizar o resourceLoader

---
+++ src/test/java/com/luisfelipe/simplecarapi/utils/FileUtils.java
@@ -0,0 +1,26 @@
+package com.luisfelipe.simplecarapi.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class FileUtils {
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    public String readResourceFile(String fileName) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:%s".formatted(fileName));
+
+        try (Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
+            return FileCopyUtils.copyToString(reader);
+        }
+    }
+}
