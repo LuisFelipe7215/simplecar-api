43dad4e4802f507cb2612b17ba11d48ddb427b90
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-11-20 11:48:36 -0300

    refactor: muda o resourceLoader para o fileUtils

--- src/test/java/com/luisfelipe/simplecarapi/controller/CarControllerTest.java
+++ src/test/java/com/luisfelipe/simplecarapi/controller/CarControllerTest.java
@@ -5,42 +5,36 @@ import com.luisfelipe.simplecarapi.exception.NotFoundException;
 import com.luisfelipe.simplecarapi.mapper.CarMapperImpl;
 import com.luisfelipe.simplecarapi.service.CarService;
 import com.luisfelipe.simplecarapi.utils.CarUtils;
+import com.luisfelipe.simplecarapi.utils.FileUtils;
 import org.junit.jupiter.api.*;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.util.FileCopyUtils;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
 
 @WebMvcTest(controllers = CarController.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Import({CarService.class, CarMapperImpl.class})
+@Import({CarService.class, CarMapperImpl.class, FileUtils.class, CarUtils.class})
 class CarControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @MockitoBean
     private CarService service;
     private List<Car> carsList;
-    @InjectMocks
+    @Autowired
     private CarUtils carUtils;
     @Autowired
-    private ResourceLoader resourceLoader;
+    private FileUtils fileUtils;
 
     @BeforeEach
     void init() {
@@ -53,7 +47,7 @@ class CarControllerTest {
     void findAll_ReturnsAllCars_WhenSuccessful() throws Exception {
         BDDMockito.given(service.findAll()).willReturn(carsList);
 
-        String response = readResourceFile("car/get-cars-200.json");
+        String response = fileUtils.readResourceFile("car/get-cars-200.json");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/cars"))
                 .andDo(MockMvcResultHandlers.print())
@@ -66,7 +60,7 @@ class CarControllerTest {
     @DisplayName("GET /v1/cars returns an empty list when no cars are found")
     void findAll_ReturnsEmptyList_WhenNoCarsAreFound() throws Exception {
         BDDMockito.given(service.findAll()).willReturn(Collections.emptyList());
-        String response = readResourceFile("car/get-empty-cars-200.json");
+        String response = fileUtils.readResourceFile("car/get-empty-cars-200.json");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/cars"))
                 .andDo(MockMvcResultHandlers.print())
@@ -81,7 +75,7 @@ class CarControllerTest {
         Car car = carsList.getFirst();
         Long id = car.getId();
         BDDMockito.given(service.findById(id)).willReturn(car);
-        String response = readResourceFile("car/get-car-by-id-200.json");
+        String response = fileUtils.readResourceFile("car/get-car-by-id-200.json");
 
         mockMvc.perform(MockMvcRequestBuilders.get("/v1/cars/{id}", id))
                 .andDo(MockMvcResultHandlers.print())
@@ -109,8 +103,8 @@ class CarControllerTest {
         Car savedCar = carUtils.newCarToSave();
         BDDMockito.given(service.save(BDDMockito.any())).willReturn(savedCar);
 
-        String request = readResourceFile("car/post-request-car-200.json");
-        String response = readResourceFile("car/post-response-car-201.json");
+        String request = fileUtils.readResourceFile("car/post-request-car-200.json");
+        String response = fileUtils.readResourceFile("car/post-response-car-201.json");
 
         mockMvc.perform(MockMvcRequestBuilders.post("/v1/cars")
                         .content(request).contentType(MediaType.APPLICATION_JSON))
@@ -127,7 +121,7 @@ class CarControllerTest {
         Car carToUpdate = carsList.getFirst().withPrice(25000D);
         BDDMockito.willDoNothing().given(service).update(carToUpdate);
 
-        String request = readResourceFile("car/put-request-car-204.json");
+        String request = fileUtils.readResourceFile("car/put-request-car-204.json");
 
         mockMvc.perform(MockMvcRequestBuilders.put("/v1/cars")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -143,7 +137,7 @@ class CarControllerTest {
         Car carToUpdate = carsList.getFirst().withPrice(25000D).withId(99L);
         BDDMockito.willThrow(new NotFoundException("Car not found")).given(service).update(carToUpdate);
 
-        String request = readResourceFile("car/put-request-car-404.json");
+        String request = fileUtils.readResourceFile("car/put-request-car-404.json");
 
         mockMvc.perform(MockMvcRequestBuilders.put("/v1/cars")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -178,11 +172,4 @@ class CarControllerTest {
                 .andExpect(MockMvcResultMatchers.status().reason("Car not found"));
     }
 
-    private String readResourceFile(String fileName) throws IOException {
-        Resource resource = resourceLoader.getResource("classpath:%s".formatted(fileName));
-
-        try (Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
-            return FileCopyUtils.copyToString(reader);
-        }
-    }
 }
