c2677e65a1e264de370ee3e306099137df85c532
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-11-21 11:57:37 -0300

    test: adiciona scripts sql do testes

---
+++ src/test/resources/sql/init_one_car.sql
@@ -0,0 +1,1 @@
+INSERT INTO car (id, brand, model, price, type, model_year) VALUES (1, 'Toyota', 'Corolla', 20000, 'SUV', 2022);
---
+++ src/test/resources/sql/init_two_cars.sql
@@ -0,0 +1,2 @@
+INSERT INTO car (id, brand, model, price, type, model_year) VALUES (1, 'Toyota', 'Corolla', 20000, 'SUV', 2022);
+INSERT INTO car (id, brand, model, price, type, model_year) VALUES (2, 'Honda', 'Civic', 20000, 'SUV', 2022);
