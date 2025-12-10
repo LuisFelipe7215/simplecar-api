dc1a6cdc7e69ab375c1b9e0a3bb7f03a4e82a2cc
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-12-10 11:08:11 -0300

    fix: remove o csrf

--- src/main/java/com/luisfelipe/simplecarapi/config/SecurityConfig.java
+++ src/main/java/com/luisfelipe/simplecarapi/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
@@ -17,11 +18,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.csrfTokenRepository(new CookieCsrfTokenRepository())
-                        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
-                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "v1/cars").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.PUT, "v1/cars").hasRole("ADMIN")
-                                .requestMatchers(HttpMethod.DELETE, "v1/cars/*").hasRole("ADMIN"))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "v1/cars").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "v1/cars").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "v1/cars/*").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "v1/users").permitAll()
+                        .anyRequest().permitAll())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
