39c80e75b785bb2ce2b0a54ea6a47a7ced632230
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-12-10 10:08:23 -0300

    feat: adiciona a conexão com a entidade user

---
+++ src/main/java/com/luisfelipe/simplecarapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.luisfelipe.simplecarapi.repository;
+
+import com.luisfelipe.simplecarapi.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
