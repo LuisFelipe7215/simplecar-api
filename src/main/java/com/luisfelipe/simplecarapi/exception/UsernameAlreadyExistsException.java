31e95d70667a33543c3580aaa0298fa4b3f62b80
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-12-11 10:52:49 -0300

    feat: adiciona a exceção de usuário existente

---
+++ src/main/java/com/luisfelipe/simplecarapi/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.luisfelipe.simplecarapi.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
