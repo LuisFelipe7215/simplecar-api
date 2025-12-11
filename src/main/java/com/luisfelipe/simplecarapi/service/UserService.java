c129ba9d68b85afbdc8fac913abe3b3c1c51ea82
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-12-11 10:53:49 -0300

    fix(service): adiciona a exceção de usuário existente

--- src/main/java/com/luisfelipe/simplecarapi/service/UserService.java
+++ src/main/java/com/luisfelipe/simplecarapi/service/UserService.java
@@ -1,6 +1,7 @@
 package com.luisfelipe.simplecarapi.service;
 
 import com.luisfelipe.simplecarapi.domain.User;
+import com.luisfelipe.simplecarapi.exception.UsernameAlreadyExistsException;
 import com.luisfelipe.simplecarapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,9 @@ public class UserService {
     private final UserRepository repository;
 
     public User save(User user){
+        if (repository.findByUsername(user.getUsername()).isPresent()){
+            throw new UsernameAlreadyExistsException("Username " + user.getUsername() + " is already being used.");
+        }
         return repository.save(user);
     }
 }
