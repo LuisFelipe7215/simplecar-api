24b54f1e323f08f5c833b60909bb9d7e601f8822
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-12-11 14:45:11 -0300

    test: adiciona os utilitários para o teste de user

---
+++ src/test/java/com/luisfelipe/simplecarapi/utils/UserUtils.java
@@ -0,0 +1,23 @@
+package com.luisfelipe.simplecarapi.utils;
+
+import com.luisfelipe.simplecarapi.domain.Car;
+import com.luisfelipe.simplecarapi.domain.User;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserUtils {
+
+    public List<User> newUsersList(){
+        User first = User.builder().id(1L).username("gustavo432").password("Golflindo123@").roles("USER").build();
+        User second = User.builder().id(1L).username("santanalover").password("Santaninha501?").roles("USER").build();
+        User third = User.builder().id(1L).username("mareadordecabeca").password("Dordecabeça404!").roles("USER").build();
+
+        return new ArrayList<>(List.of(first, second, third));
+    }
+    public User newUserToSave(){
+        return User.builder().id(1L).username("julio23").password("Teste123#").roles("USER").build();
+    }
+}
