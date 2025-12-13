86eb04e6dfad099eef3bccf75b43f468f22f7799
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-12-13 16:00:16 -0300

    feat: cria schema para a exceção username já existe

---
+++ src/main/java/com/luisfelipe/simplecarapi/exception/CustomUsernameAlreadyExistsException.java
@@ -0,0 +1,23 @@
+package com.luisfelipe.simplecarapi.exception;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Schema(description = "Default response model to API errors")
+@AllArgsConstructor
+@Getter
+@Setter
+public class CustomUsernameAlreadyExistsException {
+    @Schema(description = "Http status code", example = "409")
+    private int status;
+    @Schema(description = "Error detailed message", example = "Username already exists")
+    private String message;
+    @Schema(description = "Error date and time", example = "2025-12-13T10:30:00")
+    private LocalDateTime localDateTime;
+    @Schema(description = "Request path", example = "/v1/users")
+    private String path;
+}
