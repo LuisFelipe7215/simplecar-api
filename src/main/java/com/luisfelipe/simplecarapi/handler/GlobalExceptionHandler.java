231697c58264eed386a0a83c0d220eb5cc31024b
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-12-13 16:00:45 -0300

    feat: adiciona os schema

--- src/main/java/com/luisfelipe/simplecarapi/handler/GlobalExceptionHandler.java
+++ src/main/java/com/luisfelipe/simplecarapi/handler/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.luisfelipe.simplecarapi.handler;
 
+import com.luisfelipe.simplecarapi.exception.CustomNotFoundException;
+import com.luisfelipe.simplecarapi.exception.CustomUsernameAlreadyExistsException;
+import com.luisfelipe.simplecarapi.exception.NotFoundException;
 import com.luisfelipe.simplecarapi.exception.UsernameAlreadyExistsException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,12 +15,27 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UsernameAlreadyExistsException.class)
-    public ProblemDetail handleUsernameAlreadyExistsException(UsernameAlreadyExistsException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
-        problemDetail.setTitle("Data conflict");
-        problemDetail.setProperty("timestamp", LocalDateTime.now());
 
-        return problemDetail;
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    public ResponseEntity<CustomUsernameAlreadyExistsException> handleUsernameAlreadyExistsException(UsernameAlreadyExistsException e
+            , HttpServletRequest request) {
+        CustomUsernameAlreadyExistsException error = new CustomUsernameAlreadyExistsException(HttpStatus.CONFLICT.value(),
+                e.getMessage(),
+                LocalDateTime.now(),
+                request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<CustomNotFoundException> handleNotFoundException(NotFoundException e
+            , HttpServletRequest request) {
+        CustomNotFoundException error = new CustomNotFoundException(HttpStatus.NOT_FOUND.value(),
+                e.getMessage(),
+                LocalDateTime.now(),
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
