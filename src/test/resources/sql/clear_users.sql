c555f4d0babda09ec3f9a10d3250ddbd6c7c7cde
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-12-13 11:14:10 -0300

    refactor: remove scripts sql nao utilizados

--- src/test/resources/sql/clear_users.sql
+++
@@ -1,3 +0,0 @@
-delete
-from user
-where id <> 0;
--- src/test/resources/sql/init_one_admin.sql
+++
@@ -1,2 +0,0 @@
-insert into user (password, roles, username)
-values ('{bcrypt}$2a$10$ccecLmv1RfpUyI5Pfmed2.AwxobOzAly8852woecBsiDal3sCFxEm', 'ADMIN', 'super_admin');
