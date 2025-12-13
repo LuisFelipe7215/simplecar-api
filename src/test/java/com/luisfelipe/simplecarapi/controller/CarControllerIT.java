e87ccad09405569e1d6c51eb6a9876f3c157824b
platinex <170734954+LuisFelipe7215@users.noreply.github.com>
2025-12-13 10:06:06 -0300

    refactor: refatora o testes de integração de car

--- src/test/java/com/luisfelipe/simplecarapi/controller/CarControllerIT.java
+++ src/test/java/com/luisfelipe/simplecarapi/controller/CarControllerIT.java
@@ -1,8 +1,10 @@
 package com.luisfelipe.simplecarapi.controller;
 
+import com.luisfelipe.simplecarapi.repository.UserRepository;
 import com.luisfelipe.simplecarapi.response.CarGetResponse;
 import com.luisfelipe.simplecarapi.response.CarPostResponse;
 import com.luisfelipe.simplecarapi.utils.FileUtils;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,19 +21,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Log4j2
 public class CarControllerIT {
     public static final String URL = "/v1/cars";
+    public static final String ADMIN_USERNAME = "super_admin";
+    public static final String ADMIN_PASSWORD = "123456";
     @Autowired
     private TestRestTemplate testRestTemplate;
     @Autowired
     private FileUtils fileUtils;
+    @Autowired
+    private UserRepository repository;
 
-
+    @Order(1)
     @Test
     @DisplayName("GET /v1/cars returns all cars when successful")
     @Sql(value = "/sql/init_two_cars.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-    @Sql(value = "/sql/truncate_table.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-    @Order(1)
+    @Sql(value = "/sql/clean_cars.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void findAll_ReturnsAllCars_WhenSuccessful() {
         var typeReference = new ParameterizedTypeReference<List<CarGetResponse>>() {};
         var responseEntity = testRestTemplate.exchange(URL, HttpMethod.GET, null, typeReference);
@@ -93,10 +99,13 @@ public class CarControllerIT {
     @Order(5)
     @Test
     @DisplayName("POST /v1/car creates a car")
+    @Sql(value = "/sql/init_one_admin.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/clear_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void save_CreatesCar_WhenSuccessful() throws IOException {
         var request = fileUtils.readResourceFile("car/post-request-car-200.json");
         var carEntity = buildHttpEntity(request);
-        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.POST, carEntity, CarPostResponse.class);
+        var responseEntity = testRestTemplate.withBasicAuth(ADMIN_USERNAME, ADMIN_PASSWORD)
+                .exchange(URL, HttpMethod.POST, carEntity, CarPostResponse.class);
 
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
@@ -107,10 +116,13 @@ public class CarControllerIT {
     @Test
     @DisplayName("PUT /v1/car updates a car when successful")
     @Sql(value = "/sql/init_one_car.sql")
+    @Sql(value = "/sql/init_one_admin.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/clear_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void update_UpdateCar_WhenSuccessful() throws Exception {
         var request = fileUtils.readResourceFile("car/put-request-car-204.json");
         var carEntity = buildHttpEntity(request);
-        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.PUT, carEntity, Void.class);
+        var responseEntity = testRestTemplate.withBasicAuth(ADMIN_USERNAME, ADMIN_PASSWORD)
+                .exchange(URL, HttpMethod.PUT, carEntity, Void.class);
 
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
@@ -120,11 +132,14 @@ public class CarControllerIT {
     @Test
     @DisplayName("PUT /v1/car throws NotFoundException when car is not found")
     @Sql(value = "/sql/init_one_car.sql")
+    @Sql(value = "/sql/init_one_admin.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/clear_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void update_ThrowsNotFoundException_WhenCarIsNotFound() throws Exception {
         var request = fileUtils.readResourceFile("car/put-request-car-404.json");
         var expectedResponse = fileUtils.readResourceFile("car/put-response-car-404.json");
         var carEntity = buildHttpEntity(request);
-        var responseEntity = testRestTemplate.exchange(URL, HttpMethod.PUT, carEntity, String.class);
+        var responseEntity = testRestTemplate.withBasicAuth(ADMIN_USERNAME, ADMIN_PASSWORD)
+                .exchange(URL, HttpMethod.PUT, carEntity, String.class);
 
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
@@ -138,8 +153,10 @@ public class CarControllerIT {
     @Test
     @DisplayName("DELETE v1/car/1 removes a car when successful")
     @Sql(value = "/sql/init_one_car.sql")
+    @Sql(value = "/sql/init_one_admin.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/clear_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void delete_RemovesCar_WhenSuccessful() {
-        var responseEntity = testRestTemplate
+        var responseEntity = testRestTemplate.withBasicAuth(ADMIN_USERNAME, ADMIN_PASSWORD)
                 .exchange(URL + "/{id}", HttpMethod.DELETE, null, Void.class, 1);
 
         assertThat(responseEntity).isNotNull();
@@ -150,9 +167,11 @@ public class CarControllerIT {
     @Test
     @DisplayName("DELETE v1/car/99 throws NotFoundException when car is not found")
     @Sql(value = "/sql/init_one_car.sql")
+    @Sql(value = "/sql/init_one_admin.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = "/sql/clear_users.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     void delete_ThrowsNotFoundException_WhenCarIsNotFound() throws Exception {
         var expectedResponse = fileUtils.readResourceFile("car/delete-response-car-404.json");
-        var responseEntity = testRestTemplate
+        var responseEntity = testRestTemplate.withBasicAuth(ADMIN_USERNAME, ADMIN_PASSWORD)
                 .exchange(URL + "/{id}", HttpMethod.DELETE, null, String.class, 99);
 
         assertThat(responseEntity).isNotNull();
