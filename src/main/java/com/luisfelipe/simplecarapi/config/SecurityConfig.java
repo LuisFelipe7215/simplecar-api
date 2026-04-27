659dfb22538849c4ba8b12d67d044590192583ea
LuisFelipe7215 <170734954+LuisFelipe7215@users.noreply.github.com>
2026-04-27 14:50:54 -0300

    feat: Restinge o acesso do endpoint de atualização de fotos

--- src/main/java/com/luisfelipe/simplecarapi/config/SecurityConfig.java
+++ src/main/java/com/luisfelipe/simplecarapi/config/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PUT, "/v1/cars").hasAuthority("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/v1/cars/*").hasAuthority("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/v1/cars/*/photos").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/v1/cars/photos/*").hasAuthority("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/v1/users").permitAll()
                         .anyRequest().permitAll())
                 .httpBasic(Customizer.withDefaults())
